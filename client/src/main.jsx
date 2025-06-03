678687c79520b337d601b632abeff6c67f732b56
raushgit <raushforlife@gmail.com>
2025-06-03 12:29:29 +0530

    app and main changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { CssBaseline } from '@mui/material'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { CssBaseline } from "@mui/material";
 
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <CssBaseline/>
+    <CssBaseline />
     <App />
-  </>,
-)
+  </>
+);
