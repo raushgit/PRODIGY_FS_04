13a18c8e6dc435884efad8fd8fbc2692723c9ed8
raushgit <raushforlife@gmail.com>
2025-07-12 17:03:48 +0530

    Changes Made

--- PRODIGY_FS_04/client/src/main.jsx
+++ PRODIGY_FS_04/client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { CssBaseline } from "@mui/material";
 
+//This is the main File of the project main.jsx
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <CssBaseline />
